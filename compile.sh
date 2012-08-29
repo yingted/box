46ee9fc826c26ddee4055c1b0ec7e77885ca10d6
Ted Ying <yingted@gmail.com>
2012-08-29 14:17:44 -0400

    chroot will be setup

--- compile.sh
+++ compile.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 #usage: $0 file-blocks wall-secs cpu-secs memory-kb vsize-kb
 set -e
+exec 2>&1
+deps="`cpp -MM solution.cpp | cut -d\  -f3-`";
+if [ -n "$deps" ]
+then
+	echo "fatal error: unmet dependencies: $deps"
+	echo "compilation terminated."
+	exit 1
+fi
 objcopy --redefine-sym main=_start --redefine-sym _start=__start /usr/lib/gcc/i686-redhat-linux/4.6.3/../../../crt1.o crt1.o
 g++ solution.cpp -c -O3
 export PKG_CONFIG_PATH="`readlink -f libseccomp/lib/pkgconfig`"
@@ -8,7 +16,8 @@ export LD_LIBRARY_PATH="`readlink -f libseccomp-libseccomp/src/`"
 /usr/libexec/gcc/i686-redhat-linux/4.6.3/cc1plus -quiet -D_GNU_SOURCE box.c -quiet -dumpbase box.c -mtune=generic -march=i686 -auxbase box -O3 -o box.s `pkg-config --cflags libseccomp`
 as --32 -o box.o box.s
 /usr/libexec/gcc/i686-redhat-linux/4.6.3/collect2 --build-id --no-add-needed --eh-frame-hdr -m elf_i386 -e __start --hash-style=gnu -dynamic-linker /lib/ld-linux.so.2 -o solution /home/ted/box/crt1.o /usr/lib/gcc/i686-redhat-linux/4.6.3/../../../crti.o /usr/lib/gcc/i686-redhat-linux/4.6.3/crtbegin.o -L/usr/lib/gcc/i686-redhat-linux/4.6.3 -L/usr/lib/gcc/i686-redhat-linux/4.6.3/../../.. -L/home/ted/box/libseccomp/lib box.o -lseccomp -lstdc++ -lm -lgcc_s -lgcc -lc -lgcc_s -lgcc /usr/lib/gcc/i686-redhat-linux/4.6.3/crtend.o /usr/lib/gcc/i686-redhat-linux/4.6.3/../../../crtn.o /home/ted/box/solution.o
-rm crt1.o box.{o,s} solution.o
+#rm crt1.o box.{o,s} solution.o
+exec 2>/dev/null
 ulimit -d1024 -f${1:-1} -i5 -m${4:-$[256*1024]} -n4 -q0 -t${3:-1} -v${5:-$[320*1024]} -x0
 set +e
 time timeout ${2:-1} ./solution
