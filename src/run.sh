9e3942171fb60ff87e9072dfc1119f103e79018e
Shaun Ren <shaun.ren@linux.com>
2013-03-30 17:28:42 -0400

    support languages

--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#usage: $0 sol test_path file_blocks wall_secs cpu_secs memory_kb vsize_kb
+#usage: $0 test_path file_blocks wall_secs cpu_secs memory_kb vsize_kb
 set -e
-sol="$1"
-test_path="$2"
-lang="${test_path##*.}"
-file_blocks="${3:-1}"
-wall_secs="${4:-2}"
-cpu_secs="${5:-1}"
-memory_kb="${6:-$[256*1024]}"
-vsize_kb="${7:-$[320*1024]}"
+test_path="$1"
+file_blocks="${2:-1}"
+wall_secs="${3:-2}"
+cpu_secs="${4:-1}"
+memory_kb="${5:-$[256*1024]}"
+vsize_kb="${6:-$[320*1024]}"
 conf="/data${test_path%/*}/config"
 ext=${1%.*}
 [ -e "$conf" ] && . "$conf"
@@ -25,15 +23,39 @@ compile_cpp() {
 	exit 1
     fi
 
-    g++ solution.cpp -o solution -O3 -Wall -Wunreachable-code -march=native -pipe
-    /usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/collect2 --build-id --no-add-needed --eh-frame-hdr -m elf_x86_64 -e __start --hash-style=gnu -dynamic-linker /usr/lib/ld-linux-x86-64.so.2 -o solution /build/crt1.o /usr/lib/crti.o /usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/crtbegin.o -L/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/ -L/usr/lib/gcc/i686-redhat-linux/4.6.3/../../.. /build/box.o -L/build -lseccomp -lstdc++ -lm -lgcc_s -lgcc -lc -lgcc_s -lgcc /usr/lib/gcc/i686-redhat-linux/4.6.3/crtend.o /usr/lib/gcc/i686-redhat-linux/4.6.3/../../../crtn.o solution.o
+    g++ solution.cpp -c -O3 -Wall -Wunreachable-code -march=native -pipe
+    #/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/collect2 --build-id --no-add-needed --eh-frame-hdr -m elf_x86_64 -e __start --hash-style=gnu -dynamic-linker /usr/lib/ld-linux-x86-64.so.2 -o solution /build/crt1.o /usr/lib/crti.o /usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/crtbegin.o -L/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/ -L/usr/lib/gcc/i686-redhat-linux/4.6.3/../../.. /build/box.o -L/build -lseccomp -lstdc++ -lm -lgcc_s -lgcc -lc -lgcc_s -lgcc /usr/lib/gcc/i686-redhat-linux/4.6.3/crtend.o /usr/lib/gcc/i686-redhat-linux/4.6.3/../../../crtn.o solution.o
+     /usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/collect2 --build-id --eh-frame-hdr --hash-style=gnu -m elf_x86_64 -e __start -dynamic-linker /lib64/ld-linux-x86-64.so.2 -o solution /build/crt1.o /usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/../../../../lib/crti.o /usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/crtbegin.o -L/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2 -L/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/../../../../lib -L/lib/../lib -L/usr/lib/../lib -L/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/../../.. /build/box.o -L/build -lseccomp -lstdc++ -lm -lgcc_s -lgcc -lc -lgcc_s -lgcc /usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/crtend.o /usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/../../../../lib/crtn.o solution.o
     strip solution
 }
+compile_java() {
+    javac solution.java
+    cat > solution <<EOF
+#!/bin/bash
+java solution
+EOF
+}
+compile_py2() {
+    cat > solution <<EOF
+#!/bin/bash
+python2 -SOO solution.py2
+EOF
+}
+compile_py3() {
+    cat > solution <<EOF
+#!/bin/bash
+python3 -SOO solution.py3
+EOF
+}
+fname=`ls -1 solution.*|head -1)`
+lang=${fname##*.}
+compile_$lang
+chmod +x solution
 (
-	set +e
-	ulimit -d1024 -f"$file_blocks" -i10 -m"$memory_kb" -n10 -q0 -t"$cpu_secs" -v"$vsize_kb" -x0
-	time -o score -f "wall=%e sys=%S usr=%U cpu=%P mmax=%M rssavg=%t mavg=%t pvt=%D ss=%p ts=%X maj=%F min=%R swp=%W iow=%w in=%I out=%O" timeout "$wall_secs" taskset 0x00000001 ./solution <"/data$test_path" >stdout 2>/dev/null
-	echo $?
+    set +e
+    ulimit -d1024 -f"$file_blocks" -i10 -m"$memory_kb" -n10 -q0 -t"$cpu_secs" -v"$vsize_kb" -x0
+    time -o score -f "wall=%e sys=%S usr=%U cpu=%P mmax=%M rssavg=%t mavg=%t pvt=%D ss=%p ts=%X maj=%F min=%R swp=%W iow=%w in=%I out=%O" timeout "$wall_secs" taskset 0x00000001 ./solution <"/data$test_path" >stdout 2>/dev/null
+    echo $?
 )
 /build/score "/data$(sed 's/\(.*\)\binput\b/\1output/' <<< "$test_path")" stdout >>score
 rm solution stdout
