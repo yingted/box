517db5a238a9d37dc51288fa825bad465be98944
Shaun Ren <shaun.ren@linux.com>
2013-03-31 12:18:03 -0400

    random generators

--- src/box.c
+++ src/box.c
@@ -3,6 +3,9 @@ extern"C"{
 #include<unistd.h>
 #include<seccomp.h>
 #include<sys/mman.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 #include<stdio.h>
 extern int main(int,char*[]);
 extern int _start(int,char*[]);
@@ -11,8 +14,11 @@ int _start(int argc,char *argv[]){
 #ifndef RUN
 #define add(x,...) &&!(ret=seccomp_rule_add_exact(ctx,SCMP_ACT_ALLOW,SCMP_SYS(x),__VA_ARGS__))
 	scmp_filter_ctx ctx;
+	int fd1 = open("/dev/random", 0), fd2 = open("/dev/urandom", 0);
 	if((ctx=seccomp_init(SCMP_ACT_KILL))
 		add(read,1,SCMP_A0(SCMP_CMP_EQ,STDIN_FILENO))
+		add(read,1,SCMP_A0(SCMP_CMP_EQ,fd1))
+		add(read,1,SCMP_A0(SCMP_CMP_EQ,fd2))
 		add(write,1,SCMP_A0(SCMP_CMP_EQ,STDOUT_FILENO))
 		add(write,1,SCMP_A0(SCMP_CMP_EQ,STDERR_FILENO))
 		add(fstat64,1,SCMP_A0(SCMP_CMP_EQ,STDIN_FILENO))
