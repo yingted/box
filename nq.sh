1f457b362ef2d0796c427fdc4c083c8c980bd357
Ted Ying <yingted@gmail.com>
2013-04-01 14:25:40 -0400

    ok it works

--- nq.sh
+++ nq.sh
@@ -8,16 +8,16 @@ conf="rootfs/data${2%/*}/config"
 tconf="rootfs/data$(sed 's/\(.*\)\binput\b/\1config/' <<< "$2")"
 [ -e "$tconf" ] && . "$tconf"
 die(){
-	echo "429 $1"
+	echo 429 Too Many Requests
 	exit
 }
 mkdir -p code
 cd code
 mkdir "$1" 2>&- || find "$1" -maxdepth 0 -type d -mmin -$wait_mins >&- 2>&- || die
 trap '[ -d "$1/lock" ] && rmdir "$1/lock"' exit
-mkdir "$1/lock" 2>&- || die "AV"
+mkdir "$1/lock" 2>&- || die
 base="$1/`date +%s`"
-mkdir "$base" 2>&- || die "Enko"
+mkdir "$base" 2>&- || die
 cat > "$base/solution.$3"
 echo "$2" > "$base/in"
 echo 202 Accepted
